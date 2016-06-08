5f6002bff546cf3e2c30301c159ff701de1b18d6
Bruno Clermont <bruno@robotinfra.com>
2016-06-08 12:48:59 +0800

    update to protobuf v3

--- codec/protobuf/protobuf_test.go
+++ codec/protobuf/protobuf_test.go
@@ -3,17 +3,16 @@ package protobuf
 import (
 	"testing"
 
-	"github.com/golang/protobuf/proto"
 	"github.com/asdine/storm/codec/internal"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestProtobuf(t *testing.T) {
 	u1 := SimpleUser{
-		Id: proto.Uint64(1),
-		Name: proto.String("John"),
+		Id: uint64(1),
+		Name: "John",
 	}
 	u2 := SimpleUser{}
 	internal.RoundtripTester(t, Codec, &u1, &u2)
-	assert.True(t, u1.GetId() == u2.GetId())
+	assert.True(t, u1.Id == u2.Id)
 }
