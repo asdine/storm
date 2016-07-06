871de1809d6af162c947848741f25778d538831b
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2016-07-06 17:52:52 +0200

    Add RangeScan
    This commit also cleans up and completes the scan tests.

--- scan.go
+++ scan.go
@@ -13,7 +13,6 @@ func (s *DB) PrefixScan(prefix string) []*Node {
 
 // PrefixScan scans the buckets in this node for keys matching the given prefix.
 func (n *Node) PrefixScan(prefix string) []*Node {
-
 	if n.tx != nil {
 		return n.prefixScan(n.tx, prefix)
 	}
@@ -30,9 +29,57 @@ func (n *Node) PrefixScan(prefix string) []*Node {
 
 func (n *Node) prefixScan(tx *bolt.Tx, prefix string) []*Node {
 
-	prefixBytes := []byte(prefix)
+	var (
+		prefixBytes = []byte(prefix)
+		nodes       []*Node
+		c           = n.cursor(tx)
+	)
+
+	for k, _ := c.Seek(prefixBytes); bytes.HasPrefix(k, prefixBytes); k, _ = c.Next() {
+		nodes = append(nodes, n.From(string(k)))
+	}
+
+	return nodes
+}
+
+// RangeScan scans the root buckets over a range such as a sortable time range.
+func (s *DB) RangeScan(min, max string) []*Node {
+	return s.root.RangeScan(min, max)
+}
+
+// RangeScan scans the buckets in this node  over a range such as a sortable time range.
+func (n *Node) RangeScan(min, max string) []*Node {
+	if n.tx != nil {
+		return n.rangeScan(n.tx, min, max)
+	}
 
 	var nodes []*Node
+
+	n.s.Bolt.View(func(tx *bolt.Tx) error {
+		nodes = n.rangeScan(tx, min, max)
+		return nil
+	})
+
+	return nodes
+}
+
+func (n *Node) rangeScan(tx *bolt.Tx, min, max string) []*Node {
+	var (
+		minBytes = []byte(min)
+		maxBytes = []byte(max)
+		nodes    []*Node
+		c        = n.cursor(tx)
+	)
+
+	for k, _ := c.Seek(minBytes); k != nil && bytes.Compare(k, maxBytes) <= 0; k, _ = c.Next() {
+		nodes = append(nodes, n.From(string(k)))
+	}
+
+	return nodes
+
+}
+
+func (n *Node) cursor(tx *bolt.Tx) *bolt.Cursor {
 
 	var c *bolt.Cursor
 
@@ -42,8 +89,5 @@ func (n *Node) prefixScan(tx *bolt.Tx, prefix string) []*Node {
 		c = tx.Cursor()
 	}
 
-	for k, _ := c.Seek(prefixBytes); bytes.HasPrefix(k, prefixBytes); k, _ = c.Next() {
-		nodes = append(nodes, n.From(string(k)))
-	}
-	return nodes
+	return c
 }
