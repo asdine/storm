677925ed51b442bef41debb26776fa7a43d054a9
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2016-08-31 13:54:46 +0200

    Using json instead of gob in protobuf codec

--- codec/protobuf/protobuf.go
+++ codec/protobuf/protobuf.go
@@ -4,8 +4,8 @@ package protobuf
 import (
 	"errors"
 
+	"github.com/asdine/storm/codec/json"
 	"github.com/golang/protobuf/proto"
-	"github.com/asdine/storm/codec/gob"
 )
 
 // More details on Protocol Buffers https://github.com/golang/protobuf
@@ -21,8 +21,8 @@ type protobufCodec int
 func (c protobufCodec) Encode(v interface{}) ([]byte, error) {
 	message, ok := v.(proto.Message)
 	if !ok {
-		// toBytes() may need to encode non-protobuf type, if that occurs use gob
-		return gob.Codec.Encode(v)
+		// toBytes() may need to encode non-protobuf type, if that occurs use json
+		return json.Codec.Encode(v)
 	}
 	return proto.Marshal(message)
 }
