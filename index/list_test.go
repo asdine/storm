0b0e1b271f7da277cca7913b48d3fa66a12add65
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2016-10-11 00:45:04 +0200

    Flat list index

--- index/list_test.go
+++ index/list_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/asdine/storm"
+	"github.com/asdine/storm/codec/gob"
 	"github.com/asdine/storm/index"
 	"github.com/boltdb/bolt"
 	"github.com/stretchr/testify/assert"
@@ -47,6 +48,7 @@ func TestListIndex(t *testing.T) {
 		assert.Equal(t, index.ErrNilParam, err)
 
 		ids, err := idx.All([]byte("hello"), nil)
+		assert.NoError(t, err)
 		assert.Len(t, ids, 1)
 		assert.Equal(t, []byte("id1"), ids[0])
 
@@ -131,7 +133,8 @@ func TestListIndex(t *testing.T) {
 		id := idx.Get([]byte("hey"))
 		assert.Equal(t, []byte("id1"), id)
 
-		idx.Remove([]byte("hey"))
+		err = idx.Remove([]byte("hey"))
+		assert.NoError(t, err)
 		ids, err = idx.All([]byte("hey"), nil)
 		assert.NoError(t, err)
 		assert.Len(t, ids, 0)
@@ -160,40 +163,189 @@ func TestListIndexAddRemoveID(t *testing.T) {
 
 		err = idx.Add([]byte("hello"), []byte("id1"))
 		assert.NoError(t, err)
-		assert.Equal(t, 1, countBuckets(t, idx.IndexBucket))
+		assert.Equal(t, 1, countItems(t, idx.IndexBucket))
 
 		err = idx.Add([]byte("hello"), []byte("id2"))
 		assert.NoError(t, err)
-		assert.Equal(t, 1, countBuckets(t, idx.IndexBucket))
+		assert.Equal(t, 2, countItems(t, idx.IndexBucket))
 
 		err = idx.Add([]byte("goodbye"), []byte("id1"))
 		assert.NoError(t, err)
-		assert.Equal(t, 2, countBuckets(t, idx.IndexBucket))
+		assert.Equal(t, 2, countItems(t, idx.IndexBucket))
 
 		err = idx.Add([]byte("hello"), []byte("id3"))
 		assert.NoError(t, err)
-		assert.Equal(t, 2, countBuckets(t, idx.IndexBucket))
+		assert.Equal(t, 3, countItems(t, idx.IndexBucket))
 
 		err = idx.RemoveID([]byte("id1"))
 		assert.NoError(t, err)
-		assert.Equal(t, 1, countBuckets(t, idx.IndexBucket))
+		assert.Equal(t, 2, countItems(t, idx.IndexBucket))
 
 		err = idx.RemoveID([]byte("id2"))
 		assert.NoError(t, err)
-		assert.Equal(t, 1, countBuckets(t, idx.IndexBucket))
+		assert.Equal(t, 1, countItems(t, idx.IndexBucket))
 
 		err = idx.RemoveID([]byte("id3"))
 		assert.NoError(t, err)
-		assert.Equal(t, 0, countBuckets(t, idx.IndexBucket))
+		assert.Equal(t, 0, countItems(t, idx.IndexBucket))
 		return nil
 	})
 }
 
-func countBuckets(t *testing.T, bucket *bolt.Bucket) int {
+func TestListIndexAllRecords(t *testing.T) {
+	dir, _ := ioutil.TempDir(os.TempDir(), "storm")
+	defer os.RemoveAll(dir)
+	db, _ := storm.Open(filepath.Join(dir, "storm.db"))
+	defer db.Close()
+
+	db.Bolt.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucket([]byte("test"))
+		assert.NoError(t, err)
+
+		idx, err := index.NewListIndex(b, []byte("lindex1"))
+		assert.NoError(t, err)
+
+		ids, err := idx.AllRecords(nil)
+		assert.NoError(t, err)
+		assert.Len(t, ids, 0)
+
+		err = idx.Add([]byte("goodbye"), []byte("id2"))
+		assert.NoError(t, err)
+		assert.Equal(t, 1, countItems(t, idx.IndexBucket))
+
+		err = idx.Add([]byte("goodbye"), []byte("id1"))
+		assert.NoError(t, err)
+		assert.Equal(t, 2, countItems(t, idx.IndexBucket))
+
+		err = idx.Add([]byte("hello"), []byte("id4"))
+		assert.NoError(t, err)
+		assert.Equal(t, 3, countItems(t, idx.IndexBucket))
+
+		err = idx.Add([]byte("hello"), []byte("id3"))
+		assert.NoError(t, err)
+		assert.Equal(t, 4, countItems(t, idx.IndexBucket))
+
+		ids, err = idx.AllRecords(nil)
+		assert.NoError(t, err)
+		assert.Len(t, ids, 4)
+		assert.Equal(t, []byte("id1"), ids[0])
+		assert.Equal(t, []byte("id2"), ids[1])
+		assert.Equal(t, []byte("id3"), ids[2])
+		assert.Equal(t, []byte("id4"), ids[3])
+
+		err = idx.RemoveID([]byte("id1"))
+		assert.NoError(t, err)
+		assert.Equal(t, 3, countItems(t, idx.IndexBucket))
+
+		ids, err = idx.AllRecords(nil)
+		assert.NoError(t, err)
+		assert.Len(t, ids, 3)
+		assert.Equal(t, []byte("id2"), ids[0])
+
+		err = idx.Add([]byte("goodbye"), []byte("id1"))
+		assert.NoError(t, err)
+		assert.Equal(t, 4, countItems(t, idx.IndexBucket))
+
+		opts := index.NewOptions()
+		opts.Limit = 1
+		ids, err = idx.AllRecords(opts)
+		assert.Len(t, ids, 1)
+
+		opts = index.NewOptions()
+		opts.Skip = 2
+		ids, err = idx.AllRecords(opts)
+		assert.Len(t, ids, 2)
+
+		opts = index.NewOptions()
+		opts.Skip = 2
+		opts.Limit = 3
+		opts.Reverse = true
+		ids, err = idx.AllRecords(opts)
+		assert.NoError(t, err)
+		assert.Len(t, ids, 2)
+		assert.Equal(t, []byte("id2"), ids[0])
+
+		return nil
+	})
+}
+
+func TestListIndexRange(t *testing.T) {
+	dir, _ := ioutil.TempDir(os.TempDir(), "storm")
+	defer os.RemoveAll(dir)
+	db, _ := storm.Open(filepath.Join(dir, "storm.db"))
+	defer db.Close()
+
+	db.Bolt.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucket([]byte("test"))
+		assert.NoError(t, err)
+
+		idx, err := index.NewListIndex(b, []byte("index1"))
+		assert.NoError(t, err)
+
+		for i := 0; i < 10; i++ {
+			val, _ := gob.Codec.Marshal(i)
+			err = idx.Add(val, val)
+			assert.NoError(t, err)
+		}
+
+		min, _ := gob.Codec.Marshal(3)
+		max, _ := gob.Codec.Marshal(5)
+		list, err := idx.Range(min, max, nil)
+		assert.Len(t, list, 3)
+		assert.NoError(t, err)
+		assertEncodedIntListEqual(t, []int{3, 4, 5}, list)
+
+		min, _ = gob.Codec.Marshal(11)
+		max, _ = gob.Codec.Marshal(20)
+		list, err = idx.Range(min, max, nil)
+		assert.Len(t, list, 0)
+		assert.NoError(t, err)
+
+		min, _ = gob.Codec.Marshal(7)
+		max, _ = gob.Codec.Marshal(2)
+		list, err = idx.Range(min, max, nil)
+		assert.Len(t, list, 0)
+		assert.NoError(t, err)
+
+		min, _ = gob.Codec.Marshal(-5)
+		max, _ = gob.Codec.Marshal(2)
+		list, err = idx.Range(min, max, nil)
+		assert.Len(t, list, 0)
+		assert.NoError(t, err)
+
+		min, _ = gob.Codec.Marshal(3)
+		max, _ = gob.Codec.Marshal(7)
+		opts := index.NewOptions()
+		opts.Skip = 2
+		list, err = idx.Range(min, max, opts)
+		assert.Len(t, list, 3)
+		assert.NoError(t, err)
+		assertEncodedIntListEqual(t, []int{5, 6, 7}, list)
+
+		opts = index.NewOptions()
+		opts.Limit = 2
+		list, err = idx.Range(min, max, opts)
+		assert.Len(t, list, 2)
+		assert.NoError(t, err)
+		assertEncodedIntListEqual(t, []int{3, 4}, list)
+
+		opts = index.NewOptions()
+		opts.Reverse = true
+		opts.Skip = 2
+		opts.Limit = 2
+		list, err = idx.Range(min, max, opts)
+		assert.Len(t, list, 2)
+		assert.NoError(t, err)
+		assertEncodedIntListEqual(t, []int{5, 4}, list)
+		return nil
+	})
+}
+
+func countItems(t *testing.T, bucket *bolt.Bucket) int {
 	c := bucket.Cursor()
 	count := 0
-	for bucketName, val := c.First(); bucketName != nil; bucketName, val = c.Next() {
-		if val != nil || bytes.Equal(bucketName, []byte("storm__ids")) {
+	for k, id := c.First(); k != nil; k, id = c.Next() {
+		if id == nil || bytes.Equal(k, []byte("storm__ids")) {
 			continue
 		}
 		count++
