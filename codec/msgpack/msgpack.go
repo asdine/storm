f1fa07e8a9092ea4078262abeb924bf8a928365d
Philip Marc Schwartz <philip@progmad.com>
2017-02-07 12:05:29 -0500

    Add Msgpack codec

---
+++ codec/msgpack/msgpack.go
@@ -0,0 +1,25 @@
+// Package msgpack contains a codec to encode and decode entities in msgpack format
+package msgpack
+
+import (
+    mp "gopkg.in/vmihailenco/msgpack.v2"
+)
+
+const name = "msgpack"
+
+// Codec that encodes to and decodes from msgpack.
+var Codec = new(msgpackCodec)
+
+type msgpackCodec int
+
+func (m msgpackCodec) Marshal(v interface{}) ([]byte, error) {
+	return mp.Marshal(v)
+}
+
+func (m msgpackCodec) Unmarshal(b []byte, v interface{}) error {
+	return mp.Unmarshal(b, v)
+}
+
+func (m msgpackCodec) Name() string {
+	return name
+}
